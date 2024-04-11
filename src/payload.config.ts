f0e185ea5e21b9db96589896729ba8058e4aedaf
Elliot DeNolf <denolfe@gmail.com>
2024-04-11 12:28:28 -0400

    feat: initialize payload

---
+++ src/payload.config.ts
@@ -0,0 +1,28 @@
+import { postgresAdapter } from "@payloadcms/db-postgres";
+import { lexicalEditor } from "@payloadcms/richtext-lexical"; // editor-import
+import path from "path";
+import { buildConfig } from "payload/config";
+import { fileURLToPath } from "url";
+
+import { Users } from "./collections/Users";
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+  },
+  collections: [Users],
+  editor: lexicalEditor({}),
+  secret: process.env.PAYLOAD_SECRET || "",
+  typescript: {
+    outputFile: path.resolve(dirname, "payload-types.ts"),
+  },
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI,
+    },
+  }),
+  // TODO: Vercel Blob Storage
+});
